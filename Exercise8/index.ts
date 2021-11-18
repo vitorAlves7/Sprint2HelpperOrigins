5d02b0546f105a5dcf26144689ab7814c8d3166f
Vitor Alves <vitor.alves@helpper.com.br>
2021-11-18 07:13:41 -0300

    create function that shows if person passed is creator of the ada language

--- Exercise8/index.ts
+++ Exercise8/index.ts
@@ -26,26 +26,35 @@ let lista: Array<Person> = [
 ];
 
 class AdaLovelace extends Person {
+    private adaCreator: boolean;
     constructor(){
         super(1,'Ada Lovelace', 'Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina');
+        this.adaCreator = true;
     }
+
 }
 
 class AlanTuring extends Person {
+    private adaCreator: boolean;
     constructor() {
         super(2, 'Alan Turing', 'Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial');
+        this.adaCreator = false;
     }
 }
 
 class NikolaTesla extends Person {
+    private adaCreator: boolean;
     constructor() {
         super(3, 'Nikola Tesla', 'Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada');
+        this.adaCreator = false;
     }
 }
 
 class NicolauCopernico extends Person {
+    private adaCreator: boolean;
     constructor() {
         super(4, 'Nicolau Copérnico', 'Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar');
+        this.adaCreator = false;
     }
 }
 
@@ -77,3 +86,20 @@ class NicolauCopernicoFactory extends PersonFactory{
         return new NicolauCopernico();
     }
 }
+
+let adaLovelaceFactory: PersonFactory = new AdaLovelaceFactory();
+let ada: Person = adaLovelaceFactory.createPerson();
+
+let copernicoFactory: PersonFactory = new NicolauCopernicoFactory();
+let copernico: Person = copernicoFactory.createPerson();
+
+function creatorOfTheAdaLanguage(person: Person){
+    if(person.adaCreator === true){
+        console.log(` A pessoa ${person.name} é criador da linguagem Ada`);
+    } else {
+        console.log(` A pessoa ${person.name} não é criador da linguagem Ada`);
+    }
+}
+
+creatorOfTheAdaLanguage(copernico);
+creatorOfTheAdaLanguage(ada);
